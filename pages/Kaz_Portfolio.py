8bd26bdd4e3ad49730e3ad7c9817ae6009c50ba1
kaz-456 <idyussenov3@gatech.edu>
2024-11-30 04:48:38 +0000

    fix: fix

--- pages/Kaz_Portfolio.py
+++ pages/Kaz_Portfolio.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import Kaz_Info
 import pandas as pd
+import base64
+
+def render_image(filepath: str, width: int):
+   """
+   filepath: path to the image. Must have a valid file extension.
+   """
+   mime_type = filepath.split('.')[-1:][0].lower()
+   with open(filepath, "rb") as f:
+    content_bytes = f.read()
+    content_b64encoded = base64.b64encode(content_bytes).decode()
+    image_string = f'data:image/{mime_type};base64,{content_b64encoded}'
+    st.image(image_string, width=width)
 
 #About Me
 def about_me_section():
     st.header("About Me")
-    st.image("../profile.jpg", width=200)
+    render_image("../profile.jpg", 200)
+    # st.image("../profile.jpg", width=200)
     st.write(Kaz_Info.about_me)
     st.write('---')
 about_me_section()
